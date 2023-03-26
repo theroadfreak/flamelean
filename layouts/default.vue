6e5e6bdfcdd17171c97715017322217535b0c657
Emilija-S <emilija.simonoska@hotmail.com>
2023-03-26 22:48:55 +0200

    Navbar created

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <custom-navbar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import CustomNavbar from "../components/layouts/CustomNavbar";
+export default {
+  name: "Default",
+  components: {CustomNavbar}
+}
+</script>
+
+<style scoped>
+
+</style>
