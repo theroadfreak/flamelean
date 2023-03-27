bfaf4498c60be8e000135f8a427ce2c10db34701
Emilija Simonoska <53193642+Emilija-S@users.noreply.github.com>
2023-03-27 21:40:47 +0200

    Hero Section and Stanford Scientists Article Section created (#4)

---
+++ components/index/StanfordScientistsArticleSection.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="container py-5">
+    <div class="row d-flex justify-content-center align-items-center align-items-xxl-end">
+      <div class="order-1 order-md-0 col-12 col-lg-7 mb-md-4 mb-lg-0">
+        <div class="text-primary small">January 2023</div>
+        <h2 class="display-6 fw-bold mb-5 text-secondary">
+          Stanford Scientists Discover A Shocking New Cause of Belly Fat, And it’s Not What You Think...
+        </h2>
+        <p class="mb-0 fs-5 text-secondary pe-xxl-4">
+          Acetyl-l-carnitine; L-carnitine Carnitine is a substance that
+          helps the body turn fat into energy. Your body makes it in the
+          liver and kidneys and stores it in the skeletal muscles, heart,
+          brain, and sperm. Usually, your body can make all the
+          carnitine it needs. Some people, however, may not have
+          enough carnitine because their bodies cannot make enough
+          or cannot transport it into tissues so it can be used. Other
+          conditions, such as angina or intermittent claudication, can
+          also cause low levels of carnitine in the body, as can some
+          medications. Carnitine has been proposed as a treatment
+          for many conditions because it acts as an antioxidant.
+          Antioxidants fight harmful particles in the body known as
+          can some medications.
+        </p>
+      </div>
+      <div class="order-0 order-md-1 col-12 col-lg-5 mb-5 mb-md-0">
+        <div class="ratio ratio-2x3 max-height-80vh">
+          <img src="../../assets/images/index/stanford-scientists-article/scientist.png"
+               alt="Scientist"
+               class="object-fit-contain max-height-80vh"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "StanfordScientistsArticleSection"
+}
+</script>
+
+<style scoped>
+@media (max-width: 991.98px) {
+  .max-height-80vh {
+    max-height: 70vh;
+  }
+}
+
+@media (min-width: 1200px) {
+  .max-height-80vh {
+    max-height: 80vh;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
   <main>
-
+    <hero-section/>
+    <stanford-scientists-article-section/>
   </main>
 </template>
 
 <script>
+import HeroSection from "@/components/index/HeroSection.vue";
+import StanfordScientistsArticleSection from "@/components/index/StanfordScientistsArticleSection.vue";
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {StanfordScientistsArticleSection, HeroSection}
 }
 </script>
