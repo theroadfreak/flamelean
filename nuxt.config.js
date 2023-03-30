d049a3c1fd167ac0bc25b83e8dad2e4343a77d53
Emilija-S <emilija.simonoska@hotmail.com>
2023-03-30 14:14:41 +0200

    Updated favicon image; Added meta tags;

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,17 @@ export default {
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
       {hid: 'description', name: 'description', content: ''},
-      {name: 'format-detection', content: 'telephone=no'}
+      {name: 'format-detection', content: 'telephone=no'},
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "/meta-images/flamelean.png",
+      },
+      {
+        hid: "twitter:image",
+        name: "twitter:image",
+        content: "/meta-images/flamelean.png",
+      },
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
