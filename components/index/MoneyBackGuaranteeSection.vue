e17f492725389a7598c4fd0fbd890bf883e8995a
MartinaDonceva <doncevamartina@yahoo.com>
2023-05-06 18:26:26 +0200

    Separation line added.

--- components/index/MoneyBackGuaranteeSection.vue
+++ components/index/MoneyBackGuaranteeSection.vue
@@ -1,30 +1,33 @@
 <template>
-  <section class="container d-flex flex-column align-items-center text-center py-5">
-    <img src="../../assets/images/index/money-back-guarantee-section/money-back-guarantee-icon.svg"
-         alt="Money Back Guarantee Icon"
-         width="186"
-         height="180"
-         class="custom-dimensions-sm-screen mb-4"
-    />
-    <h1 class="custom-title-size text-primary fw-bold mb-4">
-      100% SATISFACTION <br/>
-      60-DAY MONEY BACK GUARANTEE
-    </h1>
-    <p class="mb-4 text-secondary col-12 col-lg-11 col-xl-9">
-      We promise your money back if the item you received is not as described, or if your item is not delivered within
-      the Buyer Protection period. You can get a refund 15 days after the claim process finishes. This is in
-      addition to and does not limit your statutory rights as a consumer, as granted by all mandatory
-      laws and regulations applicable in your country of residence.
-    </p>
-    <div class="col-12 col-lg-9 col-xl-7">
-      <div class="ratio ratio-83x15">
-        <img src="../../assets/images/index/money-back-guarantee-section/icons.svg"
-             alt="Icons"
-             class="object-fit-contain"
-        />
+  <section>
+    <div class="container d-flex flex-column align-items-center text-center pt-5">
+      <img src="../../assets/images/index/money-back-guarantee-section/money-back-guarantee-icon.svg"
+           alt="Money Back Guarantee Icon"
+           width="186"
+           height="180"
+           class="custom-dimensions-sm-screen mb-4"
+      />
+      <h1 class="custom-title-size text-primary fw-bold mb-4">
+        100% SATISFACTION <br/>
+        60-DAY MONEY BACK GUARANTEE
+      </h1>
+      <p class="mb-4 text-secondary col-12 col-lg-11 col-xl-9">
+        We promise your money back if the item you received is not as described, or if your item is not delivered within
+        the Buyer Protection period. You can get a refund 15 days after the claim process finishes. This is in
+        addition to and does not limit your statutory rights as a consumer, as granted by all mandatory
+        laws and regulations applicable in your country of residence.
+      </p>
+      <div class="col-12 col-lg-9 col-xl-7 mb-5">
+        <div class="ratio ratio-83x15">
+          <img src="../../assets/images/index/money-back-guarantee-section/icons.svg"
+               alt="Icons"
+               class="object-fit-contain"
+          />
+        </div>
       </div>
     </div>
-
+    <div class="col-12 border border-top border-2 border-secondary">
+    </div>
   </section>
 </template>
 
