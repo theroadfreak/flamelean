4d9bdbe15198e928dae54c0d4552f982d1e2a39b
MartinaDonceva <doncevamartina@yahoo.com>
2023-05-09 16:13:00 +0200

    Quick fixes.

--- components/no-layout-footer-pages/TextItem.vue
+++ components/no-layout-footer-pages/TextItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex flex-column align-items-start fs-5 col-lg-9 mx-auto">
+  <div class="d-flex flex-column align-items-start fs-5 col-xl-9 mx-auto">
     <div v-for="(item, i) in text" :key="i" class="mb-4">
       <div v-html="item"></div>
     </div>
