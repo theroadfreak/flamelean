8988031bc0f12485df536dece701b484320e899d
danut <matei1337@icloud.com>
2023-09-14 15:12:14 +0300

    redirect order support link to buygoods support page

--- components/layouts/CustomFooter.vue
+++ components/layouts/CustomFooter.vue
@@ -48,7 +48,7 @@ export default {
 				},
 				{
 					name: "Order Support",
-					href: "https://www.buygoods.com/",
+					href: "https://buygoods.com/contact",
 				},
 				{
 					name: "Terms",
